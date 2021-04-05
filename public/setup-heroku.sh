542dfc9e25c46075c6d6225466c9557bd4e9b265
Jeffrey Nguyen <jeffrey.nguyen98@gmail.com>
2021-04-05 18:29:50 +0700

    Create setup-heroku.sh

---
+++ public/setup-heroku.sh
@@ -0,0 +1,8 @@
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login thangnldse62897@fpt.edu.vn
+  password a8f4f414-8b90-4f3a-aa4d-6dc7e35fceae
+machine git.heroku.com
+  login thangnldse62897@fpt.edu.vn
+  password a8f4f414-8b90-4f3a-aa4d-6dc7e35fceae
+EOF
